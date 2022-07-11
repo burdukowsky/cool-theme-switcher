37f85fb8db1f25e43d371694e948cdbf1611f4c9
Stanislav Burdukowsky <burdukowskystas@gmail.com>
2022-07-11 14:17:47 +0700

    animation radius

--- js/index.js
+++ js/index.js
@@ -11,21 +11,24 @@ document.addEventListener('DOMContentLoaded', () => {
     document.head.appendChild(styleElement);
     const cssRuleIndex = 0;
 
-    function getKeyframesCss(x, y) {
+    function getKeyframesCss(x, y, radius) {
         return `@keyframes page-animation {
             0% { clip-path: circle(0% at ${x}px ${y}px); }
-            100% { clip-path: circle(100% at ${x}px ${y}px); }
+            100% { clip-path: circle(${radius}px at ${x}px ${y}px); }
         }`;
     }
 
     switcher.addEventListener('click', ({clientX, clientY}) => {
         theme = theme === LIGHT ? DARK : LIGHT;
+        const {offsetWidth: pageWidth, offsetHeight: pageHeight} = page;
+        const farthestX = (pageWidth / 2) > clientX ? pageWidth : 0;
+        const farthestY = (pageHeight / 2) > clientY ? pageHeight : 0;
+        const distance = ((farthestX - clientX) ** 2 + (farthestY - clientY) ** 2) ** .5;
+        styleElement.sheet.insertRule(getKeyframesCss(clientX, clientY, distance), cssRuleIndex);
 
         html2canvas(document.body).then(canvas => {
             canvas.classList.add('page-screenshot');
             document.body.appendChild(canvas);
-
-            styleElement.sheet.insertRule(getKeyframesCss(clientX, clientY), cssRuleIndex);
 
             page.className = theme;
             page.classList.add(pageAnimationClass);
